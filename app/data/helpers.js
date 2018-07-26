9bb8d2eb4b3d86665677cffa7b4e7b15326c3a97
Marco Laspe <marco@rockiger.com>
2018-07-26 21:48:33 +0200

    Add new helper functions hasString and thread. Also refactored position of exports.

--- app/data/helpers.js
+++ app/data/helpers.js
@@ -4,6 +4,16 @@
  */
 
 
+/***********
+ * Exports *
+ ***********/
+
+exports.checkExpect = checkExpect;
+exports.notEmpty = notEmpty;
+exports.hasString = hasString;
+exports.thread = thread;
+
+
 /******************
  * Module imports *
  ******************/
@@ -34,13 +44,12 @@ function checkExpect (obj1, obj2, msg='') {
             //console.dir("caller is " + checkExpect.caller);
             console.log("==============================")
             console.log("Test:", msg, "failed.")
-            console.dir(obj1, "is not equal to", obj2);
+            console.log(obj1, "\nis not equal to\n", obj2);
             console.log("called from:", _.last(error.stack.split("\n")[1].split("/")).slice(0, -1));
             console.log("==============================")
         }
     }
 }
-exports.checkExpect = checkExpect;
 
 
 function notEmpty(list) {
@@ -48,4 +57,58 @@ function notEmpty(list) {
 }
 checkExpect(notEmpty([]), false);
 checkExpect(notEmpty([1]), true);
-exports.notEmpty = notEmpty; 
+
+/**
+ * Determins if a string contains a substring
+ * @param {string} str - the string to be searched
+ * @param {string} ss - the search string
+ */
+function hasString(str, ss) {
+    const result = str.search(new RegExp(ss, "i"));
+    return (result !== -1) ? true : false;
+}
+checkExpect(hasString("Test", "t"), true);
+checkExpect(hasString("Test", "T"), true);
+checkExpect(hasString("Test", "o"), false);
+
+
+/**
+ * Thread function in a more readable way, similar to the clojure threading macros
+ * Read more at:
+ *      https://clojuredocs.org/clojure.core/->
+ *      https://clojuredocs.org/clojure.core/->>
+ * @param {string} operator - the threading operator, -> or ->>
+ * @param {*} first 
+ * @param {function|Array} args - one ore more function(s) and/or Arrays
+ * @returns {*} - the result of the computation
+ * thread("->", "3",
+ *  fn1,
+ *  [fn2, val],
+ *  fn3) 
+ */
+function thread(operator, first, ...args) {
+    let isThreadFirst;
+    switch (operator) {
+        case '->>':
+            isThreadFirst = false;
+            break
+        case '->':
+            isThreadFirst = true;
+            break;
+        default:
+            throw new Error('Operator not supported');
+            break;
+    }
+    return args.reduce((prev, next) => {
+        if (Array.isArray(next)) {
+            const [head, ...tail] = next;
+            return isThreadFirst ? head.apply(this, [prev, ...tail]) : head.apply(this, tail.concat(prev));
+        }
+        else {
+            return next.call(this, prev);
+        }
+    }, first);
+}
+checkExpect(thread("->", "3",
+                parseInt),
+            3);
